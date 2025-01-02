bfeccf90c0e982f7becccd262d7e5b5175bdd0a9
Kyle Bridgford <57777806+kbridgford@users.noreply.github.com>
2025-01-02 22:04:12 +0000

    updated url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kbridgford.github.io/kbridgford/kapawa-web',
+  site: 'https://kbridgford.github.io/kapawa-web/',
   integrations: [tailwind()]
 });
