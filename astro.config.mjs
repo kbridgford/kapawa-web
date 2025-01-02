ea0663ed1be0594e0afeea33bfd752841a9378ad
Kyle Bridgford <57777806+kbridgford@users.noreply.github.com>
2025-01-02 22:28:49 +0000

    custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kbridgford.github.io',
-  base: 'kapawa-web',
+  site: 'https://kapawa.net',
   integrations: [tailwind()]
 });
